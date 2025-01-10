c30f45092a48a59986f463a87fa3d41e2cc30742
iiblueblue <58199536+iiblueblue@users.noreply.github.com>
2025-01-11 02:46:02 +0900

    update : SpawnManger 구현

--- Text_RPG/CSpawnManager.cpp
+++ Text_RPG/CSpawnManager.cpp
@@ -1,2 +1,40 @@
 #include "pch.h"
 #include "CSpawnManager.h"
+#include <random>
+
+std::unique_ptr<CMonster> CSpawnManager::GenerateMonster(int level)
+{
+	std::default_random_engine RandomGenerator;
+	// 체력은 캐릭터 레벨에 비례해서 랜덤하게 생성(레벨*20~레벨*30)
+	std::uniform_int_distribution<int> MonsterDistribution(0, 2);
+	int MonsterIndex = MonsterDistribution(RandomGenerator);
+
+	switch (MonsterIndex)
+	{
+	case 0:
+		Factory = std::make_unique<CGoblinFactory>();
+		break;
+	case 1:
+		Factory = std::make_unique<COrcFactory>();
+		break;
+	case 2:
+		Factory = std::make_unique<CTrollFactory>();
+		break;
+	}
+	return Factory->CreateMonster(level);
+}
+
+std::unique_ptr<CBossMonster> CSpawnManager::GenerateBossMonster(int level)
+{
+	Factory = std::make_unique<CBossMonsterFactory>();
+	// TODO : 나중에 확인 형변환
+	auto monster = Factory->CreateMonster(level);
+
+	CBossMonster* boss = dynamic_cast<CBossMonster*>(monster.get());
+	if (boss == nullptr) {
+		throw std::runtime_error("Generated monster is not a CBossMonster");
+	}
+
+	monster.release();
+	return std::unique_ptr<CBossMonster>(boss);
+}
--- Text_RPG/CSpawnManager.h
+++ Text_RPG/CSpawnManager.h
@@ -1,7 +1,21 @@
 #pragma once
+#include <memory>
+#include "CGoblin.h"
+#include "COrc.h"
+#include "CTroll.h"
+#include "CBossMonster.h"
+#include "CGoblinFactory.h"
+#include "COrcFactory.h"
+#include "CTrollFactory.h"
+#include "CBossMonsterFactory.h"
+
 class CSpawnManager :
     public CSingleton<CSpawnManager>
 {
-
+private:
+    std::unique_ptr<CMonsterFactory> Factory;
+public:
+    std::unique_ptr<CMonster> GenerateMonster(int level);
+    std::unique_ptr<CBossMonster> GenerateBossMonster(int level);
 };
 
