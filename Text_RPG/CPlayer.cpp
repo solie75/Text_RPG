766acceba1b5fa9af48d5866c389eb3fea1ab2b8
JangJinWoo <aktsksqkq@gmail.com>
2025-01-13 11:58:37 +0900

    add : 생성자, 이름넣기

--- Text_RPG/CPlayer.cpp
+++ Text_RPG/CPlayer.cpp
@@ -1,2 +1,38 @@
 #include "pch.h"
 #include "CPlayer.h"
+#include <iostream>
+
+CPlayer::CPlayer()
+{
+	Level = 1;
+	MaxHealth = 200;
+	Health = MaxHealth;
+	Damage = 30;
+	Experience = 0;
+	Gold = 0;
+}
+
+void CPlayer::SetName(string _str)
+{
+	Name = _str;
+}
+
+void CPlayer::LevelUp()
+{
+}
+
+void CPlayer::TakeDamage(int)
+{
+}
+
+void CPlayer::DisplayStatus()
+{
+}
+
+void CPlayer::UseItem(int)
+{
+}
+
+void CPlayer::VisitShop()
+{
+}
--- Text_RPG/CPlayer.h
+++ Text_RPG/CPlayer.h
@@ -1,6 +1,28 @@
 #pragma once
 class CPlayer :
-    public CSingleton<CPlayer>
+	public CSingleton<CPlayer>
 {
+	SINGLE(CPlayer)
+
+private:
+	string Name;
+	int Level;
+	int Health;
+	int MaxHealth;
+	int Damage;
+	int Experience;
+	int Gold;
+	//vector<item*> Inventory;
+
+	//get, set
+public:
+	void CPlayer();
+	void SetName(string);
+public:
+	void LevelUp();
+	void TakeDamage(int);
+	void DisplayStatus();
+	void UseItem(int);
+	void VisitShop();
 };
 
