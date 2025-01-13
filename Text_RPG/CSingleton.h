b17d91274ec7beddf443ca294994ed7a34d96e7a
JoongEun Shin <solie0705@gmail.com>
2025-01-13 13:01:55 +0900

    Update Singleton

--- Text_RPG/CSingleton.h
+++ Text_RPG/CSingleton.h
@@ -5,39 +5,14 @@ typedef void (*EXIT)(void);
 template<typename T>
 class CSingleton
 {
-private:
-	static T* m_Inst;
-
 public:
-	static T* GetInst();
-	static void Destroy();
+	static T* GetInst()
+	{
+		static T* instance = new T;
+		return instance;
+	};
 
 protected:
 	CSingleton() {};
 	virtual ~CSingleton() {};
 };
-
-template<typename T>
-T* CSingleton<T>::m_Inst = nullptr;
-
-template<typename T>
-inline T* CSingleton<T>::GetInst()
-{
-	if (nullptr == m_Inst)
-	{
-		m_Inst = new T;
-		atexit((EXIT)&CSingleton<T>::Destroy);
-	}
-
-	return m_Inst;
-}
-
-template<typename T>
-inline void CSingleton<T>::Destroy()
-{
-	if (nullptr != m_Inst)
-	{
-		delete m_Inst;
-		m_Inst = nullptr;
-	}
-}
