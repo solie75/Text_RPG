5cbc27ddf8e83d91bb9b7bac4024be1104e321b1
gadada <aug120jw@gmail.com>
2025-01-14 14:07:52 +0900

    update: startstage

--- Text_RPG/CStartStage.cpp
+++ Text_RPG/CStartStage.cpp
@@ -1,2 +1,90 @@
 #include "pch.h"
+#include <string>
 #include "CStartStage.h"
+#include "CKeyManager.h"
+#include "CStageManager.h"
+#include "CDungeonStage.h"
+#include "CPlayer.h"
+
+void CStartStage::StageInit()
+{
+
+}
+
+void CStartStage::StageTick()
+{
+	if (bCallRender)
+	{
+		StageRender();
+		bCallRender = false;
+	}
+
+	if (CKeyManager::GetInst()->GetKeyState(KEY_TYPE::SPACE) == KEY_STATE::TAP)
+	{
+		CStageManager::GetInst()->ChangeStage(new CDungeonStage());
+	}
+}
+
+void CStartStage::StageRender()
+{
+	std::cout << "\033[2J\033[H";
+	
+	string PlayerID;
+	CPlayer* Player = CPlayer::GetInst();
+	Player->SetName(PlayerID);
+
+	//줄 맞추기용 임시 프린트
+	//printf("□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□\n");
+
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                               _     _  _______  ___      ___      _______  _______  __   __  _______  __                                                               \n");
+	printf("                                                              | | _ | ||       ||   |    |   |    |       ||       ||  |_|  ||       ||  |                                                              \n");
+	printf("                                                              | || || ||    ___||   |    |   |    |      _||   _   ||       ||    ___||  |                                                              \n");
+	printf("                                                              |       ||   |___ |   |    |   |    |     |  |  | |  ||       ||   |___ |  |                                                              \n");
+	printf("                                                              |       ||    ___||   |___ |   |___ |     |_ |  |_|  ||       ||    ___||__|                                                              \n");
+	printf("                                                              |   _   ||   |___ |       ||       ||       ||       || ||_|| ||   |___  __                                                               \n");
+	printf("                                                              |__| |__||_______||_______||_______||_______||_______||_|   |_||_______||__|                                                              \n");
+	printf("                                                                                __   __  _______  ______    _______  __                                                                                 \n");
+	printf("                                                                               |  | |  ||       ||    _ |  |       ||  |                                                                                \n");
+	printf("                                                                               |  |_|  ||    ___||   | ||  |   _   ||  |                                                                                \n");
+	printf("                                                                               |       ||   |___ |   |_||_ |  | |  ||  |                                                                                \n");
+	printf("                                                                               |       ||    ___||    __  ||  |_|  ||__|                                                                                \n");
+	printf("                                                                               |   _   ||   |___ |   |  | ||       | __                                                                                 \n");
+	printf("                                                                               |__| |__||_______||___|  |_||_______||__|                                                                                \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                     Please type your PlayerID                                                                                          \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                         ");std::getline(std::cin, PlayerID);
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                     Press Space Bar to Start!                                                                                          \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        \n");
+	printf("                                                                                                                                                                                                        ");
+}
+
--- Text_RPG/CStartStage.h
+++ Text_RPG/CStartStage.h
@@ -1,7 +1,15 @@
 #pragma once
 #include "CStage.h"
+#include "CStageManager.h"
+
 class CStartStage :
     public CStage
 {
+private:
+    bool bCallRender = true;
+public:
+    virtual void StageInit() override;
+    virtual void StageTick() override;
+    virtual void StageRender() override;
 };
 
