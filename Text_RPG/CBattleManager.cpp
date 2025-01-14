9c24a8358817ab27255929018b1a00176b1364c4
iiblueblue <58199536+iiblueblue@users.noreply.github.com>
2025-01-14 15:26:16 +0900

    comment : 주석 수정

--- Text_RPG/CBattleManager.cpp
+++ Text_RPG/CBattleManager.cpp
@@ -4,10 +4,11 @@
 
 void CBattleManager::Battle(CPlayer& player, CMonster& monster)
 {
+	// 랜럼 관련 변수들
 	std::default_random_engine RandomGenerator;
-	std::uniform_int_distribution<int> ItemUseDistribution(0, 100);
-	std::uniform_int_distribution<int> MonsterAttackDistribution(0, 100);
-	std::uniform_int_distribution<int> PlayerAttackDistribution(0, 100);
+	std::uniform_int_distribution<int> ItemUseDistribution(0, 100); // 아이템 사용 난수 생성
+	std::uniform_int_distribution<int> MonsterAttackDistribution(0, 100); // 몬스터 공격 난수 생성
+	std::uniform_int_distribution<int> PlayerAttackDistribution(0, 100); // 플레이어 공격 난수 생성
 
 	// 몬스터 등장 텍스트
 	while (monster.GetHealth() > 0 && player.GetHealth() > 0) // 플레이어 체력이나 몬스터의 체력이 0이될 때가지
@@ -21,16 +22,22 @@ void CBattleManager::Battle(CPlayer& player, CMonster& monster)
 		}
 		else if (ItemUseProbabiliity > 50)
 		{
-			player.UseItem("attackboost"); // 25% 확률로 회복 아이템 사용
+			player.UseItem("attackboost"); // 25% 확률로 공격력 증가 아이템 사용
 		}
 
 		// 플레이어의 공격
 		int PlayerAttackProbability = PlayerAttackDistribution(RandomGenerator);
-		if (PlayerAttackProbability > 50)
+		if (PlayerAttackProbability > 50) // 50% 확률로 공격 성공
 		{
 			// 공격 성공
 			monster.Hit(player.GetDamage()); // 몬스터 데미지 입음
 			// 공격 성공 텍스트
+
+			// 지금 공격으로 몬스터가 죽었다면
+			if (monster.GetHealth() <= 0)
+			{
+				break; // 몬스터 턴으로 가지 않고 반복 종료
+			}
 		}
 		else
 		{
@@ -41,7 +48,7 @@ void CBattleManager::Battle(CPlayer& player, CMonster& monster)
 		// [몬스터의 턴]
 		// 몬스터의 공격
 		int MonsterAttackProbability = MonsterAttackDistribution(RandomGenerator);
-		if (MonsterAttackProbability > 50)
+		if (MonsterAttackProbability > 50) // 50% 확률로 공격 성공
 		{
 			// 공격 성공
 			player.Hit(monster.GetDamage()); // 플레이어 데미지 입음
