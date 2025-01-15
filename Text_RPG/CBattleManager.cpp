b21ed93188cf29cf0132017250fc20b84db06e7a
iiblueblue <58199536+iiblueblue@users.noreply.github.com>
2025-01-15 16:00:40 +0900

    fix : Random 수정

--- Text_RPG/CBattleManager.cpp
+++ Text_RPG/CBattleManager.cpp
@@ -5,17 +5,16 @@
 void CBattleManager::Battle(CPlayer& player, CMonster& monster)
 {
 	// 랜럼 관련 변수들
-	std::default_random_engine RandomGenerator;
-	std::uniform_int_distribution<int> ItemUseDistribution(0, 100); // 아이템 사용 난수 생성
-	std::uniform_int_distribution<int> MonsterAttackDistribution(0, 100); // 몬스터 공격 난수 생성
-	std::uniform_int_distribution<int> PlayerAttackDistribution(0, 100); // 플레이어 공격 난수 생성
+	std::random_device RandomDevice; // 시드값을 얻기 위한 random_device 생성
+	std::mt19937 gen(RandomDevice()); // random_device를 통해 난수 생성 엔진을 초기화
+	std::uniform_int_distribution<int> Distribution(0, 100); // 0~100까지 난수열을 생성하귀 위해 균등 분포 정의
 
 	// 몬스터 등장 텍스트
 	while (monster.GetHealth() > 0 && player.GetHealth() > 0) // 플레이어 체력이나 몬스터의 체력이 0이될 때가지
 	{
 		// [플레이어의 턴]
 		// 플레이어의 아이템 사용
-		int ItemUseProbabiliity = ItemUseDistribution(RandomGenerator);
+		int ItemUseProbabiliity = Distribution(gen);
 		if (ItemUseProbabiliity > 75) 
 		{
 			player.UseItem(ITEM_TYPE::HEALTH_POTION); // 25% 확률로 회복 아이템 사용
@@ -26,7 +25,7 @@ void CBattleManager::Battle(CPlayer& player, CMonster& monster)
 		}
 
 		// 플레이어의 공격
-		int PlayerAttackProbability = PlayerAttackDistribution(RandomGenerator);
+		int PlayerAttackProbability = Distribution(gen);
 		if (PlayerAttackProbability > 50) // 50% 확률로 공격 성공
 		{
 			// 공격 성공
@@ -47,7 +46,7 @@ void CBattleManager::Battle(CPlayer& player, CMonster& monster)
 
 		// [몬스터의 턴]
 		// 몬스터의 공격
-		int MonsterAttackProbability = MonsterAttackDistribution(RandomGenerator);
+		int MonsterAttackProbability = Distribution(gen);
 		if (MonsterAttackProbability > 50) // 50% 확률로 공격 성공
 		{
 			// 공격 성공
