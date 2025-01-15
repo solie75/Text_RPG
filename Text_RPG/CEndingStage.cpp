54954c96af24358c195897a403f679f5750ba1d2
gadada <aug120jw@gmail.com>
2025-01-15 23:21:47 +0900

    update_itemCntprint
    아이템갯수(상점, 플레이어) 랜더에 프린팅하는 함수 추가

--- Text_RPG/CEndingStage.cpp
+++ Text_RPG/CEndingStage.cpp
@@ -2,6 +2,7 @@
 #include "CEndingStage.h"
 #include "CStartStage.h"
 #include "CKeyManager.h"
+#include <stdlib.h>
 
 void CEndingStage::StageInit()
 {
@@ -25,6 +26,70 @@ void CEndingStage::StageTick()
 
 }
 
+	if (CKeyManager::GetInst()->GetKeyState(KEY_TYPE::ESC) == KEY_STATE::TAP) //esc 누르면 콘솔 종료
+	{		
+		exit(0);
+	}
+
+	//for (int i = 0; KEY_TYPE::ESC != KEY_STATE::TAP; ++i )
+	//{
+	//	CKeyManager::GetInst()->GetKeyState(i)
+	//	CStageManager::GetInst()->ChangeStage(new CStartStage());
+	//}
+
+}
+
 void CEndingStage::StageRender()
 {
+	std::cout << "\033[2J\033[H";
+	printf("□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□                                                                                                                                                                                                    □\n");
+	printf("□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□");
 }
--- Text_RPG/CShopStage.h
+++ Text_RPG/CShopStage.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "CStage.h"
+#include "CShopManager.h"
+#include "CPlayer.h"
+
 class CShopStage :
     public CStage
 {
@@ -12,6 +15,17 @@ public:
 private:
     bool bCallRender = true;
     BUYSELL_MODE CurPlayer_Mode = BUYSELL_MODE::BUY;
+    int shopPotionCnt = CShopManager::GetInst()->GetItemCnt(ITEM_TYPE::HEALTH_POTION);
+    int shopBoostCnt = CShopManager::GetInst()->GetItemCnt(ITEM_TYPE::ATTACK_BOOST);
+    string zeroBUYPotionCnt; //just zero for print, buy potion
+    string zeroBUYBoostCnt; //just zero for print, buy boost
+    int PlayerPotionCnt = CPlayer::GetInst()->GetItemCnt(ITEM_TYPE::HEALTH_POTION);
+    int PlayerBoostCnt = CPlayer::GetInst()->GetItemCnt(ITEM_TYPE::ATTACK_BOOST);
+    int PlayerMonLeatherCnt = CPlayer::GetInst()->GetItemCnt(ITEM_TYPE::MONSTER_LEATHER);
+    string zeroSELLPotionCnt; // just zero for print, sell potion
+    string zeroSELLBoostCnt; // just zero for print, sell boost
+    string zeroSELLMonLeatherCnt; //just zero for print, sell monster leather
+
 
 public:
     virtual void StageInit() override;
